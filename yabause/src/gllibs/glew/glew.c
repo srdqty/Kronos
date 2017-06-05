0821c724d0b1fa44a33938aa8863281ef56b2f75
LeCare <francois.caron.perso@gmail.com>
2017-06-05 21:25:37 +0200

    Fix glew potential isue

--- yabause/src/gllibs/glew/glew.c
+++ yabause/src/gllibs/glew/glew.c
@@ -14258,6 +14258,7 @@ GLenum glxewContextInit (GLXEW_CONTEXT_ARG_DEF_LIST)
   GLXEW_VERSION_1_3 = GL_TRUE;
   GLXEW_VERSION_1_4 = GL_TRUE;
   /* query GLX version */
+  if ((glXGetCurrentDisplay != NULL) && (glXGetCurrentDisplay() != 0)) {
   glXQueryVersion(glXGetCurrentDisplay(), &major, &minor);
   if (major == 1 && minor <= 3)
   {
@@ -14274,6 +14275,7 @@ GLenum glxewContextInit (GLXEW_CONTEXT_ARG_DEF_LIST)
       return GLEW_ERROR_GLX_VERSION_11_ONLY;
       break;
     }
+  }
   }
   /* query GLX extension string */
   extStart = 0;

c98b49f76066ed8918fc502da8faa5c424bde6ae
LeCare <francois.caron.perso@gmail.com>
2018-02-28 22:00:22 +0100

    Switch on CORE_PROFILE

--- yabause/src/linux/platform/glfw/platform.c
+++ yabause/src/linux/platform/glfw/platform.c
@@ -149,12 +149,12 @@ int platform_SetupOpenGL(int w, int h, int fullscreen) {
 #ifdef _OGLES3_
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API) ;
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 #else
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);  
 #endif
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_RED_BITS,8);
   glfwWindowHint(GLFW_GREEN_BITS,8);
